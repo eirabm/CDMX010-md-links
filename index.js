914d839e4bb9ba25e46baf26fb8561c864403380
Eira Mazon <eira.b.mazon@gmail.com>
2021-03-22 16:58:04 -0600

    feat(index): function that reads a file and prints every link with href and text

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-module.exports = () => {
+//module.exports = () => {
   // ...
-};
+//};
+
+const fs = require('fs');
+const path = require('path');
+const marked = require("marked");
+
+let links = [];
+fs.readFile('./README.md', (err, data) => {
+if (err) {console.log(err)}
+document = data.toString();
+
+/*const walkTokens = (token) => {
+  if (token.type === 'link'){
+    console.log(token)
+  } else {''}
+}
+ marked.use({walkTokens})*/
+
+ const renderer = new marked.Renderer();
+ renderer.link = (href, title, text) => {
+   links.push({ href, title, text })
+ }
+
+ marked.use({ renderer });
+
+marked(document);
+
+links.forEach((link) => {
+  console.log(link.href, link.text)
+})
+});
+
+
+
+  const thisPath = path.parse('./README.md');
+
+  console.log(thisPath.ext)
+
+
+
